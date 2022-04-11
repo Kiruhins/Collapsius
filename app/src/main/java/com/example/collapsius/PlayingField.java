045769d0d8f4ab989ba67d24b578447abd0306d7
Kirill <k.mazhara1@g.nsu.ru>
2022-04-11 21:52:08 +0700

    1

--- app/src/main/java/com/example/collapsius/PlayingField.java
+++ app/src/main/java/com/example/collapsius/PlayingField.java
@@ -6,22 +6,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+class Cells {
+    public int[][] cellsmas;
+    public int[][] player;
+
+
+    public Cells(int[][] cellmass,int[][] player){
+        this.cellsmas=cellmass;
+        this.player=player;
+    }
+
+}
+
+
 public class PlayingField extends AppCompatActivity implements View.OnClickListener {
 
-    int[][] cellsmas= new int[5][5];
-    int[][] player= new int[5][5];
+    int[][] cellsmas= new int[8][8];
+    int[][] player= new int[8][8];
     Cells cell;
-    public void addButtons(){
-        // Это встроенный метод в Java, который перерисует все объекты в окне(если его не прописывать, то объекты не будут видны(в нашем случае это кнопки))
-        for (int j = 0; j < cell.cellsmas.length; j++) {
-            for (int i = 0; i < cell.cellsmas[j].length; i++) {
-                //Button b.findViewById(R.id.bt_classic);
-
-            }
-        }
-
-
-    }
 
 
     @Override
@@ -30,22 +32,17 @@ public class PlayingField extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_playing_field);
 
         cell = new Cells(cellsmas, player);
+
+        for (int i=0;i<cell.cellsmas.length;++i){
+            for (int j=0;j<cell.cellsmas.length;++j){
+                cell.cellsmas[i][j]=0;
+                cell.player[i][j]=0;
+            }
+        }
     }
 
     @Override
     public void onClick(View view) {
 
     }
-}
-class Cells {
-    public int[][] cellsmas;
-    public int[][] player;
-    public int i=0;
-
-
-    public Cells(int[][] cellmass,int[][] player){
-        this.cellsmas=cellmass;
-        this.player=player;
-    }
-
 }
