e063927d49ccdd65df8a28a81518f75a7f8f2925
White4ndBlack <v.tkachev@g.nsu.ru>
2022-05-24 08:02:43 +0700

    added configuration for editable

---
+++ app/src/main/java/com/example/collapsius/configuration_editable.java
@@ -0,0 +1,14 @@
+package com.example.collapsius;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class configuration_editable extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuration_editable);
+    }
+}
