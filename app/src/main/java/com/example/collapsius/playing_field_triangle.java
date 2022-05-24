e698618459ff38897af69dfe3e5c14d52ccb7e64
White4ndBlack <v.tkachev@g.nsu.ru>
2022-05-24 08:01:18 +0700

    added map for triangle

---
+++ app/src/main/java/com/example/collapsius/playing_field_triangle.java
@@ -0,0 +1,14 @@
+package com.example.collapsius;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class playing_field_triangle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playing_field_triangle);
+    }
+}
